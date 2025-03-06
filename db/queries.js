ff569fb1364829eb2cebe60f32c1a8b279062c19
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-03-06 23:05:23 +0400

    Added 2 crucial scripts into package.json - this setup can serve as a template for other projects

---
+++ db/queries.js
@@ -0,0 +1,23 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: {
+      published: true,
+    },
+  });
+  console.log(post);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect;
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect;
+    process.exit(1);
+  });
